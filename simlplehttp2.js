d826babe53e7112da1c7590546b40c496d185902
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-02 15:49:33 -0400

    Added a promise, and  moved the fetch there.

--- simlplehttp2.js
+++ simlplehttp2.js
@@ -1,8 +1,10 @@
 class SimpleHTTP {
   get(url) {
-    fetch(url)
-      .then(response => response.json())
-      .then(data => data)
-      .catch(error => error);
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => error);
+    });
   }
 }
