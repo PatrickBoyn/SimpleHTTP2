6d20893d2fe1d03823898adb439385a8940232ed
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-02 17:47:32 -0400

    Commented out the employee. Also changed the url in the post.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = new SimpleHTTP();
-const employee = {
-  firstName: 'Mathew',
-  lastName: 'Appple',
-  gender: 'Male',
-  dateOfBirth: '1975-11-05',
-  phoneNumber: '1-555-5555',
-  email: 'test2@gmail.com'
-};
+// const employee = {
+//   firstName: 'Mathew',
+//   lastName: 'Appple',
+//   gender: 'Male',
+//   dateOfBirth: '1975-11-05',
+//   phoneNumber: '1-555-5555',
+//   email: 'test2@gmail.com'
+// };
 // Get all employees
 // http
 //   .get('http://localhost:5000/api/employee')
@@ -18,3 +18,9 @@ const employee = {
 //   .get('http://localhost:5000/api/employee/1')
 //   .then(data => console.log(data))
 //   .catch(error => console.log(error));
+
+// Post an employee record
+http
+  .post('http://localhost:5000/api/employee/new', employee)
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
