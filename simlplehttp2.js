021121acbbcd81203aaa9672f4de6cded612dd37
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-02 15:47:20 -0400

    Removed the console log.

--- simlplehttp2.js
+++ simlplehttp2.js
@@ -2,7 +2,7 @@ class SimpleHTTP {
   get(url) {
     fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.log(error));
+      .then(data => data)
+      .catch(error => error);
   }
 }
