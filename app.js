25c829965b00a51327d16d5eff9204f9dffed22f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-02 17:16:05 -0400

    Added an employee to delete.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const http = new SimpleHTTP();
-
+const employee = {
+  id: 4,
+  firstName: 'John',
+  lastName: 'Doe',
+  gender: 'Male',
+  dateOfBirth: '1981-01-13',
+  phoneNumber: '1-555-5555',
+  email: 'test@gmail.com'
+};
 // Get all employees
 // http
 //   .get('http://localhost:5000/api/employee')
