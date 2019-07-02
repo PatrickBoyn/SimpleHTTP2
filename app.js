d3296ce503d1c6364053c7b4795e72f75774e842
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-02 16:49:22 -0400

    Commented out the single employee get request.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const http = new SimpleHTTP();
 //   .catch(error => console.log(error));
 
 // Get single employee
-http
-  .get('http://localhost:5000/api/employee/1')
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+// http
+//   .get('http://localhost:5000/api/employee/1')
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
