151b5578541cbecb411f2f099a68f72f7c4c79b1
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-02 15:56:03 -0400

    Added a then, and catch to be able to read the data properly from the promise.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const http = new SimpleHTTP();
 
-http.get('http://localhost:5000/api/employee');
+http
+  .get('http://localhost:5000/api/employee')
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
